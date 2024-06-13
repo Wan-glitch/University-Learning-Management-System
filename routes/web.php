99bf6573a1bbea86048369b78527c5949c0bf9d7
Irfan <devzon3@gmail.com>
2024-06-14 03:45:06 +0800

    Calendar, fac n etc

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\FacultyController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Auth\ProviderController;
-use App\Http\Controllers\FacultyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,17 +44,34 @@ Route::middleware('auth')->group(function () {
 
     Route::prefix('faculties')->group(function () {
         Route::get('/', [FacultyController::class, 'index'])->name('faculty');
-        Route::post('/', [FacultyController::class, 'store'])->name('faculties.store');
+        Route::get('/{faculty}', [FacultyController::class, 'detail'])->name('faculty.detail');
+        Route::post('/faculties', [FacultyController::class, 'store'])->name('faculties.store');
+
+
     });
 
     Route::prefix('calendar')->group(function () {
         Route::get('/', [CalendarController::class, 'index'])->name('calendar.index');
         Route::post('/create-event', [CalendarController::class, 'store'])->name('calendar.store');
-
+        Route::get('/events', [CalendarController::class, 'events'])->name('calendar.events');
+        Route::get('/event', [CalendarController::class, 'event'])->name('calendar.event');
         Route::delete('/event/{id}', [CalendarController::class, 'destroy'])->name('calendar.destroy');
+        Route::post('/event-invitation/response', [CalendarController::class, 'respond'])->name('event-invitation.respond');
     });
 
+    Route::prefix('admin')->group(function () {
+        // Route::get('/', [UserController::class, 'index'])->name('admin.users.index');
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        Route::get('/users/{user}', [UserController::class, 'show'])->name('admin.users.show');
+        Route::post('/users', [UserController::class, 'store'])->name('admin.users.store');
+        Route::post('/users/batch', [UserController::class, 'batchStore'])->name('admin.users.batchStore');
+        Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
+    });
 
+    Route::get('/users/export', [UserController::class, 'export'])->name('users.export');
+
+    Route::get('/get-initial-suggestions', [UserController::class, 'getInitialSuggestions'])->name('get-initial-suggestions');
+    Route::get('/get-user', [UserController::class, 'getUserSuggestions'])->name('get-user');
 });
 
 
