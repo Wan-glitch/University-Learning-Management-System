83d7402840df0ec59879c6693fd897566ff3b3cb
Irfan <devzon3@gmail.com>
2024-06-14 11:34:39 +0800

    Added Role / Faculty relay

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Role;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 
 class User extends Authenticatable
@@ -43,4 +44,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function GotRole()
+    {
+        return $this->belongsTo(Role::class, 'role'); // References `role` column
+    }
+
+    public function HasFaculty()
+    {
+        return $this->belongsTo(Faculty::class, 'faculty'); // References `faculty` column
+    }
 }
