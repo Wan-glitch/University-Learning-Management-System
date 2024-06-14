80847a719fb0e1c77218f3d9dbba40e2efa76705
Irfan <devzon3@gmail.com>
2024-06-14 11:11:48 +0800

    changed to boolean status

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_no')->unique()->nullable();
-            $table->integer('user_status')->default(1);;
+            $table->boolean('user_status')->default(1);;
             $table->unsignedBigInteger('role')->nullable()->default(null);
             $table->unsignedBigInteger('faculty')->nullable()->default(null);
             $table->foreignId('created_by')->nullable()->constrained('users');
